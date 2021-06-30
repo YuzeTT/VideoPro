81a1624ad25f8bfb28ab52eecb068674bb0f7090
YuzeTT <ziv1024@163.com>
2021-07-01 01:17:09 +0800

    基础框架完成

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
+      <v-app-bar :color="$vuetify.theme.dark ? 'dark' : 'white'" flat>
+        <v-toolbar-title>VideoPro</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <div v-if="$vuetify.breakpoint.name !== 'xs'">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on">
+                <v-icon>{{ svgPath.mdiAccount }}</v-icon>
+              </v-btn>
+            </template>
+            <span>管理</span>
+          </v-tooltip>
+        </div>
+        <div v-else>
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </div>
+      </v-app-bar>
+      <div class="px-3">
+        <v-alert
+          icon="mdi-information-outline"
+          class="mb-1"
+          dense
+          
+          type="info"
+        >
+          当前为Beta版本，推荐使用稳定版
+        </v-alert>
+      </div>
+      
       <router-view/>
+     
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import { mdiAccount } from '@mdi/js'
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    svgPath: {mdiAccount},
+    tab: ''
   }),
 };
 </script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,67 @@
 <template>
-  <hello-world />
+  <!-- <hello-world /> -->
+  <div>
+    <v-tabs
+      v-model="tab"
+      fixed-tabs
+      :background-color="$vuetify.theme.dark ? 'dark' : 'white'"
+      :color="$vuetify.theme.dark ? 'white' : 'dark'"
+    >
+      <v-tab>
+        概览
+      </v-tab>
+      <v-tab>
+        添加/修改
+      </v-tab>
+      <v-tab>
+        调试
+      </v-tab>
+      
+      
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              概览
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              概览1
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>
+              概览2
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  // import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      // HelloWorld,
     },
+    data: () => ({
+      tab: 0
+    }),
   }
 </script>
